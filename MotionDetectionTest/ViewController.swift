4f7fc01f9c2f654bade1cfdcc6b129e84f57eeaf
Jakub Tomanik <j.tomanik@gmail.com>
2021-02-05 16:03:59 +0100

    Initial Commit

---
+++ MotionDetectionTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MotionDetectionTest
+//
+//  Created by Jakub Tomanik on 05/02/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
